88da3330afb0f2a747322c79c794f6c5171de550
Javid <none>
2021-12-16 21:29:14 +0330

    [Feature] SignUp Page

---
+++ src/Components/SignUp/SignUp.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const SignUp = () => {
+    return <div className="mt-4">
+        <div className="card-body bg-gray-600 shadow-lg rounded-box w-96">
+            <div className="form-control">
+                <label className="label">
+                    <span className="label-text">Email</span>
+                </label>
+                <input type="text" placeholder="Email" className="input input-bordered"/>
+            </div>
+            <div className="form-control mt-5">
+                <label className="label">
+                    <span className="label-text">Password</span>
+                </label>
+                <input type="password" placeholder="Password" className="input input-bordered"/>
+            </div>
+            <div className="form-control mt-5">
+                <label className="label">
+                    <span className="label-text">Confirm Password</span>
+                </label>
+                <input type="password" placeholder="Confirm Password" className="input input-bordered"/>
+            </div>
+            <div className="form-control mt-8">
+                <button className="btn btn-primary">
+                    Register
+                </button>
+            </div>
+
+        </div>
+    </div>
+}
+export default SignUp;
