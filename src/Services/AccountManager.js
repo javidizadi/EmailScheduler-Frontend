d5cf6bac7bef2394324ad100353c31c38b33782c
Javid izadi <javid.izadi1384@gmail.com>
2021-12-20 23:29:40 +0330

    [Refactor] Account Manager Service

--- src/Services/AccountManager.js
+++ src/Services/AccountManager.js
@@ -1,59 +1,66 @@
-import {client, isAuthed, Auth, clearAuth} from "./http-client";
+import { client, AuthCheck, Auth, clearAuth } from "./http-client";
 
 export const Login = async (email, password) => {
-    await client
-        .post("/login", {email: email, password: password})
+  try {
+    let response = await client.post("/login", {
+      email: email,
+      password: password,
+    });
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 
-        .then((response) => {
-            if (response.status === 200) {
-                Auth(response.data["token"]);
-                return true;
-            } else {
-                console.log(response.data["errors"]);
-                return false;
-            }
-        })
-        .catch((error) => {
-            console.log(error);
-            return false;
-        });
+  if (response.status === 200) {
+    Auth(response.data["token"]);
+    return true;
+  } else {
+    console.log(response.data["errors"]);
+    return false;
+  }
 };
 
 export const Register = async (email, password) => {
-    await client
-        .post("/register", {email: email, password: password}).then((response) => {
+  let response;
+  try {
+    response = await client.post("/register", {
+      email: email,
+      password: password,
+    });
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 
-            if (response.status === 200) {
-                Auth(response.data["token"]);
-                return true;
-            } else {
-                console.log(response.data["errors"]);
-                return false;
-            }
-        })
-        .catch((error) => {
-            console.log(error);
-            return false;
-        });
-}
+  if (response.status === 200) {
+    Auth(response.data["token"]);
+    return true;
+  } else {
+    console.log(response.data["errors"]);
+    return false;
+  }
+};
 
 export const ChangePassword = async (currentPassword, newPassword) => {
-    if (isAuthed === true) {
-        await client
-            .post("/ChangePassword", {currentPassword: currentPassword, newPassword: newPassword})
-            .then((response) => {
-                if (response.status === 200) {
-                    return true;
-                } else if (response.status === 401) {
-                    clearAuth();
-                } else {
-                    console.log(response.data);
-                    return false;
-                }
-            })
-            .catch(error => {
-                console.log(error);
-                return false;
-            });
+  if (AuthCheck === true) {
+    let response;
+    try {
+      response = await client.post("/ChangePassword", {
+        currentPassword: currentPassword,
+        newPassword: newPassword,
+      });
+    } catch (error) {
+      console.log(error);
+      return false;
     }
-}
+
+    if (response.status === 200) {
+      return true;
+    } else if (response.status === 401) {
+      clearAuth();
+    } else {
+      console.log(response.data);
+      return false;
+    }
+  }
+};
