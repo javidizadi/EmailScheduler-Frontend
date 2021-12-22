55aad1a363c85827e76e5d7b1c43a8c7d66af885
Javid izadi <javid.izadi1384@gmail.com>
2021-12-23 00:44:48 +0330

    [Feature] User Data Validation

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,28 +1,99 @@
-import React from "react";
+import React, {useState} from "react";
+import validator from "validator/es";
+import {Register as sendRegister} from "../../Services/AccountManager";
 
 const SignUp = () => {
+
+    const [emailValid, setEmailValid] = useState(false);
+
+    const handleValidationEmail = (event) => {
+        setEmailValid(validator.isEmail(event.target.value));
+    };
+
+    const [passwordValid, setPasswordValid] = useState(false);
+
+    const handleValidationPassword = (event) => {
+        setPasswordValid(validator.isStrongPassword(event.target.value));
+    };
+
+    const [passwordMatch, setPasswordMatch] = useState(false);
+
+    const handleMatchPasswords = (event) => {
+        setPasswordMatch(event.target.value === document.getElementById("password").value);
+    }
+
+    const handleSubmit = async () => {
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+
+        if (emailValid && passwordValid && passwordMatch) {
+            const result = await sendRegister(email, password);
+            if (result.isSucceed) {
+                // ToDo : completed When Routing is initialized
+            } else {
+                alert(result.result);
+            }
+        }
+    };
+
     return <div className="mt-4">
         <div className="card-body bg-gray-600 shadow-lg rounded-box w-96">
             <div className="form-control">
                 <label className="label">
                     <span className="label-text">Email</span>
                 </label>
-                <input type="text" placeholder="Email" className="input input-bordered"/>
+                <input id="email" type="text" placeholder="Email" className="input input-bordered"
+                       onChange={handleValidationEmail}/>
+                {emailValid ? null : (
+                    <label className="label">
+              <span className="label-text-alt text-red-400">
+                Please Enter Valid Email
+              </span>
+                    </label>
+                )}
             </div>
             <div className="form-control mt-5">
                 <label className="label">
                     <span className="label-text">Password</span>
                 </label>
-                <input type="password" placeholder="Password" className="input input-bordered"/>
+                <input id="password" type="password" placeholder="Password" onChange={handleValidationPassword}
+                       className="input input-bordered"/>
+                {passwordValid ? null : (
+                    <label className="label">
+                        <ul>
+                            <li className="label-text">Your Password Must:</li>
+                            <li className="label-text-alt text-red-400">
+                                Contains Alphabet
+                            </li>
+                            <li className="label-text-alt text-red-400">
+                                Contains Capital Alphabet
+                            </li>
+                            <li className="label-text-alt text-red-400">
+                                Contains Number
+                            </li>
+                            <li className="label-text-alt text-red-400">
+                                At least 8 Characters
+                            </li>
+                        </ul>
+                    </label>
+                )}
             </div>
             <div className="form-control mt-5">
                 <label className="label">
                     <span className="label-text">Confirm Password</span>
                 </label>
-                <input type="password" placeholder="Confirm Password" className="input input-bordered"/>
+                <input type="password" placeholder="Confirm Password" className="input input-bordered"
+                       onChange={handleMatchPasswords}/>
+                {passwordMatch ? null : (
+                    <label className="label">
+              <span className="label-text-alt text-red-400">
+                Passwords Dont Match.
+              </span>
+                    </label>
+                )}
             </div>
             <div className="form-control mt-8">
-                <button className="btn btn-primary">
+                <button onClick={handleSubmit} className="btn btn-primary">
                     Register
                 </button>
             </div>
