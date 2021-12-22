7711fff5bc4afbee16873b52b1b3cb383fed9cbf
Javid izadi <javid.izadi1384@gmail.com>
2021-12-22 19:36:05 +0330

    [Feature] response template object

--- src/Services/http-client.js
+++ src/Services/http-client.js
@@ -2,6 +2,11 @@ import axios from "axios";
 
 export let client = axios.create({ baseURL: "https://localhost:7143" }); // it's temp for dev process
 
+export const response = {
+  isSucceed: false,
+  result: null,
+};
+
 export const CheckAuth = () => {
   return client.defaults.headers.common["Authorization"] !== "";
 };
